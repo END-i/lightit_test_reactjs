6d230282f18761eb7dc2d7863b6ff4cfbd296747
END-i <evgen.donev@gmail.com>
2021-08-04 16:04:39 +0300

    eslint shadow rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 0,
 
     'no-console': ['error', { allow: ['log', 'warn', 'error'] }],
-    'no-shadow': ['warn'],
+    // note you must disable the base rule as it can report incorrect errors
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
   },
 };
